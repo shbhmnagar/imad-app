962e5ffe44b411991718385d93c10c162fd1fb08
Shubham Nagar <31824884+shbhmnagar@users.noreply.github.com>
2018-02-18 22:46:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,6 +101,16 @@ app.get('/counter', function(req, res) {
    res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function(req, res) { //URL: /submit-name?name=xxxx
+    // Get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);
+    // JSON: Javascript Pbject Notation
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function(req, res) {
     // articleName == article-one
     // articles[articleName] == {} content object for article-number
@@ -118,16 +128,6 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names = [];
-app.get('/submit-name', function(req, res) { //URL: /submit-name?name=xxxx
-    // Get the name from the request
-    var name = req.query.name;
-    
-    names.push(name);
-    // JSON: Javascript Pbject Notation
-    res.send(JSON.stringify(names));
 });
 
 
